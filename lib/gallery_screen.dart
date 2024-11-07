cae76ec34481ae5ca93f6efae5998b832e247577
jahirnoriega <noriegajahir@gmail.com>
2024-11-07 10:39:22 -0600

    gallery screen completed with images!

--- lib/gallery_screen.dart
+++ lib/gallery_screen.dart
@@ -1,14 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:kiyomi/components/header.dart';
+import 'styles/colors.dart';
 
 class GalleryScreen extends StatelessWidget {
+  final List<String> imagePaths = [
+    'images/class.jpeg',
+    'images/bathrooms.jpeg',
+    'images/conference.jpeg',
+    'images/school.jpeg',
+    'images/class_board.jpeg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Gallery'),
+      backgroundColor: AppColors.background,
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Collage de imágenes
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                ),
+                itemCount: imagePaths.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      _openImage(context, imagePaths[index]);
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset(
+                        imagePaths[index],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            // Leyenda debajo del collage
+            Container(
+              padding: const EdgeInsets.all(13),
+              color: AppColors.primary,
+              width: double.infinity,
+              child: const Text(
+                'Screenshots taken from the game.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 40),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('Gallery Screen'),
+    );
+  }
+
+  void _openImage(BuildContext context, String imagePath) {
+    Navigator.of(context).push(PageRouteBuilder(
+      opaque: false,
+      pageBuilder: (BuildContext context, _, __) {
+        return FullScreenImage(imagePath: imagePath);
+      },
+      transitionsBuilder: (_, animation, __, child) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+    ));
+  }
+}
+
+class FullScreenImage extends StatelessWidget {
+  final String imagePath;
+
+  const FullScreenImage({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black.withOpacity(0.8), // Fondo oscuro
+      body: Stack(
+        children: [
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 40,
+            right: 20,
+            child: IconButton(
+              icon: const Icon(Icons.close, color: Colors.white, size: 30),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
